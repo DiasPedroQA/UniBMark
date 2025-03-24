b7f3e29be63c6615244b301a285544e94de48c0b
DiasPedroQA <diaspedro.dev@gmail.com>
2025-03-23 23:55:15 -0300

    refatora o pipeline de automação para instalar o Black antes da verificação de formatação e remove expressões regulares não utilizadas
    Signed-off-by: DiasPedroQA <diaspedro.dev@gmail.com>

--- src/config/building.py
+++
@@ -1,7 +0,0 @@
-import re
-
-# Expressões regulares para identificar caminhos
-REGEX_CAMINHO_ABSOLUTO = re.compile(
-    r"^(?:[A-Z]:\\|/|\w+:/).*", re.IGNORECASE
-)  # Windows & Unix
-REGEX_CAMINHO_RELATIVO = re.compile(r"^(?![A-Z]:\\|/|\w+:).+")  # Caminho relativo
